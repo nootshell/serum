2aecee4a445d616a1689aa88b0b9c7b1608aae77
ice cube tray <icecubetray@users.noreply.github.com>
2018-07-19 23:41:53 +0200

    CI properly configured, fix wonky script

--- sbin/test-siot.sh
+++ sbin/test-siot.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+EXECUTABLE="$1";
+if [ ! -x "$EXECUTABLE" ]; then
+	if [ ! -x 'bin/test' ]; then
+		echo "Couldn't find test executable.";
+		exit 10;
+	else
+		EXECUTABLE='bin/test';
+	fi;
+fi;
+
 # Enable job control.
 set -m;
 
 # Start up the server.
-bin/test siot-server &
+"$EXECUTABLE" siot-server &
 
 # Give it a second to start up.
 sleep 1s;
 
 # Send the input file's contents to the server, and compare it to what we expect.
-cat 'test/siot/input.txt' | bin/test siot-client | diff 'test/siot/output.txt' -
+cat 'test/siot/input.txt' | "$EXECUTABLE" siot-client | diff 'test/siot/output.txt' -
 
 # Check if we got what we expected (0, equal).
 RESULT=$?
