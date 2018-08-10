bb4860abd23d74fccac5be043c66ec091e0800fd
ice cube tray <icecubetray@users.noreply.github.com>
2018-08-10 22:59:38 +0200

    Brush up test/main.c

--- test/main.c
+++ test/main.c
@@ -1,47 +1,47 @@
-/*******************************************************************************
-**                                                                            **
-**   The MIT License                                                          **
-**                                                                            **
-**   Copyright 2017 icecubetray                                               **
-**                                                                            **
-**   Permission is hereby granted, free of charge, to any person              **
-**   obtaining a copy of this software and associated documentation files     **
-**   (the "Software"), to deal in the Software without restriction,           **
-**   including without limitation the rights to use, copy, modify, merge,     **
-**   publish, distribute, sublicense, and/or sell copies of the Software,     **
-**   and to permit persons to whom the Software is furnished to do so,        **
-**   subject to the following conditions:                                     **
-**                                                                            **
-**   The above copyright notice and this permission notice shall be           **
-**   included in all copies or substantial portions of the Software.          **
-**                                                                            **
-**   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,          **
-**   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF       **
-**   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.   **
-**   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY     **
-**   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,     **
-**   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE        **
-**   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                   **
-**                                                                            **
-*******************************************************************************/
+/******************************************************************************
+**                                                                           **
+**   The MIT License                                                         **
+**                                                                           **
+**   Copyright 2017-2018 icecubetray                                         **
+**                                                                           **
+**   Permission is hereby granted, free of charge, to any person             **
+**   obtaining a copy of this software and associated documentation files    **
+**   (the "Software"), to deal in the Software without restriction,          **
+**   including without limitation the rights to use, copy, modify, merge,    **
+**   publish, distribute, sublicense, and/or sell copies of the Software,    **
+**   and to permit persons to whom the Software is furnished to do so,       **
+**   subject to the following conditions:                                    **
+**                                                                           **
+**   The above copyright notice and this permission notice shall be          **
+**   included in all copies or substantial portions of the Software.         **
+**                                                                           **
+**   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,         **
+**   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF      **
+**   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  **
+**   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY    **
+**   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,    **
+**   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE       **
+**   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                  **
+**                                                                           **
+******************************************************************************/
 
 
 #undef LS_EXPORTING
 
 #include "./unit/bitops.h"
 
-#include "../libserum/io/log.h"
+#include <libserum/io/log.h>
 
-#include "../libserum/crypto/__selftests/base.h"
-#include "../libserum/crypto/registry.h"
+#include <libserum/crypto/__selftests/base.h>
+#include <libserum/crypto/registry.h>
 
 #include <string.h>
 
 
 
 
-int __run_siot_server();
-int __run_siot_client();
+int __run_siot_server(void);
+int __run_siot_client(void);
 
 
 int
@@ -95,7 +95,7 @@ main(int argc, char *argv[], char *env[]) {
 
 	const struct {
 		char param[32];
-		int (*function)();
+		int (*function)(void);
 	} __paramtable[] = {
 		{
 			.param = "run-cst",
