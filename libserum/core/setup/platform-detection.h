dfe4d35f9b067f4e9f7691dcc63babfdc101d4a8
ice cube tray <icecubetray@users.noreply.github.com>
2018-08-13 05:14:02 +0200

    Document core/setup/platform-detection.h

--- libserum/core/setup/platform-detection.h
+++ libserum/core/setup/platform-detection.h
@@ -34,14 +34,21 @@
 
 #include <limits.h>
 
-#ifdef CHAR_BIT
+#if (defined(CHAR_BIT) || LS_DOXYGEN)
 #	if (CHAR_BIT != 8)
 #		error Currently only platforms with 8-bit characters are supported.
 #	endif
 
+/*! \hideinitializer \brief Number of bits in a byte. */
 #	define LS_BITS_BYTE						CHAR_BIT
+
+/*! \hideinitializer \brief Number of bits in a word. */
 #	define LS_BITS_WORD						(LS_BITS_BYTE << 1)
+
+/*! \hideinitializer \brief Number of bits in a double-word. */
 #	define LS_BITS_DWORD					(LS_BITS_BYTE << 2)
+
+/*! \hideinitializer \brief Number of bits in a quad-word. */
 #	define LS_BITS_QWORD					(LS_BITS_BYTE << 3)
 #else
 #	error Missing preprocessor definition: CHAR_BIT
@@ -50,9 +57,11 @@
 
 
 
+//! \cond LS_DOXYGEN_PRIVATE
 #define LS_ID_X86							1
 #define LS_ID_X64							2
 #define LS_ID_ARM							3
+//! \endcond
 
 #ifndef LS_ID_ARCH
 #	if (defined(i386) || defined(__i386) || defined(__i386__) || defined(__IA32__) || defined(_M_I86) || defined(_M_IX86) || defined(__X86__) || defined(_X86_) || defined(__THW_INTEL__) || defined(__I86__) || defined(__INTEL__) || defined(__386))
@@ -88,22 +97,34 @@
 #	endif
 #endif
 
+
 #if (LS_ID_ARCH == LS_ID_X86)
+/*! \hideinitializer \brief Defined and set to `1` if the detected platform is \em x86. */
 #	define LS_X86							1
-#elif (LS_ID_ARCH == LS_ID_X64)
+#endif
+
+#if (LS_ID_ARCH == LS_ID_X64)
+/*! \hideinitializer \brief Defined and set to `1` if the detected platform is \em x64. */
 #	define LS_X64							1
-#elif (LS_ID_ARCH == LS_ID_ARM)
+#endif
+
+#if (LS_ID_ARCH == LS_ID_ARM)
+/*! \hideinitializer \brief Defined and set to `1` if the detected platform is \em ARM. */
 #	define LS_ARM							1
-#else
+#endif
+
+#if (!LS_X86 && !LS_X64 && !LS_ARM)
 #	error Unknown architecture.
 #endif
 
 
 
 
+//! \cond LS_DOXYGEN_PRIVATE
 #define LS_ID_LINUX							1
 #define LS_ID_WINDOWS						2
 #define LS_ID_MAC							3
+//! \endcond
 
 #ifndef LS_ID_OS
 #	if (defined(__gnu_linux__) || defined(__linux__) || defined(linux))
@@ -115,21 +136,33 @@
 #	endif
 #endif
 
+
 #if (LS_ID_OS == LS_ID_LINUX)
+/*! \hideinitializer \brief Defined and set to `1` if the detected OS is \em Linux. */
 #	define LS_LINUX							1
-#elif (LS_ID_OS == LS_ID_WINDOWS)
+#endif
+
+#if (LS_ID_OS == LS_ID_WINDOWS)
+/*! \hideinitializer \brief Defined and set to `1` if the detected OS is \em Windows. */
 #	define LS_WINDOWS						1
-#elif (LS_ID_OS == LS_ID_MAC)
+#endif
+
+#if (LS_ID_OS == LS_ID_MAC)
+/*! \hideinitializer \brief Defined and set to `1` if the detected OS is \em Mac. */
 #	define LS_MAC							1
-#else
+#endif
+
+#if (!LS_LINUX && !LS_WINDOWS && !LS_MAC)
 #	error Unknown operating system.
 #endif
 
 
 
 
+//! \cond LS_DOXYGEN_PRIVATE
 #define LS_ID_LITTLE_ENDIAN					1
 #define LS_ID_BIG_ENDIAN					2
+//! \endcond
 
 #ifndef LS_ID_ENDIAN
 #	if (defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) || defined(__AARCH64EB__) || defined(_MIPSEB) || defined(__MIPSEB) || defined(__MIPSEB__))
@@ -142,27 +175,34 @@
 #		else
 #			error Unsupported endianness.
 #		endif
-#	else
-#		define LS_ID_ENDIAN					LS_ID_LITTLE_ENDIAN
 #	endif
 #endif
 
+
 #if (LS_ID_ENDIAN == LS_ID_BIG_ENDIAN)
+/*! \hideinitializer \brief Defined and set to `1` if the detected endianness is \em big-endian. */
 #	define LS_BIG_ENDIAN					1
-#elif (LS_ID_ENDIAN == LS_ID_LITTLE_ENDIAN)
+#endif
+
+#if (LS_ID_ENDIAN == LS_ID_LITTLE_ENDIAN)
+/*! \hideinitializer \brief Defined and set to `1` if the detected endianness is \em little-endian. */
 #	define LS_LITTLE_ENDIAN					1
-#else
+#endif
+
+#if (!LS_BIG_ENDIAN && !LS_LITTLE_ENDIAN)
 #	error Unknown endianness.
 #endif
 
 
 
 
+//! \cond LS_DOXYGEN_PRIVATE
 #define LS_ID_GCC							1
 #define LS_ID_LLVM							2
 #define LS_ID_MSC							3
 #define LS_ID_MINGW							4
 #define LS_ID_TCC							5
+//! \endcond
 
 #ifndef LS_ID_COMPILER
 #	if (defined(__llvm__) || defined(__clang__))
@@ -178,21 +218,34 @@
 #	endif
 #endif
 
+
 #if ((LS_ID_COMPILER == LS_ID_GCC) || (LS_ID_COMPILER == LS_ID_LLVM))
 #	if (LS_ID_COMPILER == LS_ID_GCC)
+/*! \hideinitializer \brief Defined and set to `1` if the detected compiler is \em GCC. */
 #		define LS_GCC						1
 #	else
+/*! \hideinitializer \brief Defined and set to `1` if the detected compiler is \em LLVM. */
 #		define LS_LLVM						1
 #	endif
-#elif (LS_ID_COMPILER == LS_ID_MINGW)
+#endif
+
+#if (LS_ID_COMPILER == LS_ID_MINGW)
+/*! \hideinitializer \brief Defined and set to `1` if the detected compiler is \em MinGW. */
 #	define LS_MINGW							1
-#elif (LS_ID_COMPILER == LS_ID_MSC)
+#endif
+
+#if (LS_ID_COMPILER == LS_ID_MSC)
+/*! \hideinitializer \brief Defined and set to `1` if the detected compiler is \em MSC. */
 #	define LS_MSC							1
-#elif (LS_ID_COMPILER == LS_ID_TCC)
+#endif
+
+#if (LS_ID_COMPILER == LS_ID_TCC)
+/*! \hideinitializer \brief Defined and set to `1` if the detected compiler is \em TCC. */
 #	define LS_TCC							1
 #endif
 
-#if (defined(_UCRT))
+#if (defined(_UCRT) || LS_DOXYGEN)
+/*! \hideinitializer \brief Defined and set to `1` if the _Universal C Runtime_ is enabled (Windows). */
 #	define LS_UCRT							1
 #endif
 
