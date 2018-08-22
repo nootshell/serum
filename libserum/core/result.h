b9a4448e28fbada046afdcdfd0e8f8e0060d42c1
ice cube tray <icecubetray@users.noreply.github.com>
2018-08-22 19:52:19 +0200

    Fix multi-line documentation markers used for single-line documentation

--- libserum/core/result.h
+++ libserum/core/result.h
@@ -38,88 +38,88 @@
 
 
 
-/*! \brief Successful. */
+//! \brief Successful.
 #define LS_E_SUCCESS						0x00
 
-/*! \brief Failure. */
+//! \brief Failure.
 #define LS_E_FAILURE						0x01
 
-/*! \brief Null-pointer encountered. */
+//! \brief Null-pointer encountered.
 #define LS_E_NULL							0x02
 
-/*! \brief Parameter or state/data invalid. */
+//! \brief Parameter or state/data invalid.
 #define LS_E_INVALID						0x03
 
-/*! \brief Operation not supported (not yet implemented). */
+//! \brief Operation not supported (not yet implemented).
 #define LS_E_UNSUPPORTED					0x04
 
-/*! \brief Magic value found (or not) where inappropriate. */
+//! \brief Magic value found (or not) where inappropriate.
 #define LS_E_MAGIC							0x05
 
-/*! \brief Operation timed out. */
+//! \brief Operation timed out.
 #define LS_E_TIMEOUT						0x06
 
-/*! \brief Mutex/operation was abandoned. */
+//! \brief Mutex/operation was abandoned.
 #define LS_E_ABANDONED						0x07
 
-/*! \brief Size parameter/data invalid. */
+//! \brief Size parameter/data invalid.
 #define LS_E_SIZE							0x08
 
-/*! \brief Object is in an uninitialized state. */
+//! \brief Object is in an uninitialized state.
 #define LS_E_UNINITIALIZED					0x09
 
-/*! \brief Conversion failure. */
+//! \brief Conversion failure.
 #define LS_E_CONVERSION						0x0A
 
-/*! \brief Mutex-related failure. */
+//! \brief Mutex-related failure.
 #define LS_E_LOCK							0x0B
 
-/*! \brief Object is in an invalid state. */
+//! \brief Object is in an invalid state.
 #define LS_E_STATE							0x0C
 
-/*! \brief The operation would have no effect. */
+//! \brief The operation would have no effect.
 #define LS_E_NOOP							0x0D
 
-/*! \brief Memory-related failure. */
+//! \brief Memory-related failure.
 #define LS_E_MEMORY							0x0E
 
-/*! \brief Operation is an unnecessary repetition. */
+//! \brief Operation is an unnecessary repetition.
 #define LS_E_ALREADY						0x0F
 
-/*! \brief IO error related to closing. */
+//! \brief IO error related to closing.
 #define LS_E_IO_CLOSE						0x10
 
-/*! \brief Target IO stream invalid. */
+//! \brief Target IO stream invalid.
 #define LS_E_IO_TARGET						0x11
 
-/*! \brief IO error related to writing. */
+//! \brief IO error related to writing.
 #define LS_E_IO_WRITE						0x12
 
-/*! \brief IO error related to flushing. */
+//! \brief IO error related to flushing.
 #define LS_E_IO_FLUSH						0x13
 
-/*! \brief Index out of bounds. */
+//! \brief Index out of bounds.
 #define LS_E_BOUNDS							0x14
 
-/*! \brief Object or item not found. */
+//! \brief Object or item not found.
 #define LS_E_NOT_FOUND						0x15
 
-/*! \brief Object seems to be in an inoperable state. */
+//! \brief Object seems to be in an inoperable state.
 #define LS_E_INOPERABLE						0x16
 
-/*! \brief Object seems to be out of sync. */
+//! \brief Object seems to be out of sync.
 #define LS_E_OUT_OF_SYNC					0x17
 
-/*! \brief Initialization failure. */
+//! \brief Initialization failure.
 #define LS_E_INITIALIZATION					0x18
 
-/*! \brief Data did not match what was expected. */
+//! \brief Data did not match what was expected.
 #define LS_E_DATA_MISMATCH					0x19
 
-/*! \brief Algorithm error. */
+//! \brief Algorithm error.
 #define LS_E_ALGORITHM						0x1A
 
-/*! \brief Counter depleted. */
+//! \brief Counter depleted.
 #define LS_E_DEPLETED						0x1B
 
 
