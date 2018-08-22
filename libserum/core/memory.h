c6708d276006149fd37d1cd05cf433ff7e3fcdc4
ice cube tray <icecubetray@users.noreply.github.com>
2018-08-22 18:50:49 +0200

    Document core/memory.h

--- libserum/core/memory.h
+++ libserum/core/memory.h
@@ -39,7 +39,10 @@
 
 
 #if (LS_GCC || LS_LLVM || LS_TCC)
+#	//! \hideinitializer \brief Allocates an array on the stack.
 #	define LS_STACK_ALLOC(type, var, sizeb)	type var[(sizeb)]
+#
+#	//! \hideinitializer \brief Deallocates an array from the stack that was previously allocated by `#LS_STACK_ALLOC`.
 #	define LS_STACK_FREE(var)
 #endif
 
@@ -60,52 +63,69 @@ extern "C" {
 #endif
 
 	/*!
-	 * \brief ls_memory_dump_ex
-	 * \param src
-	 * \param size
-	 * \param columns
-	 * \param columnsize
-	 * \param dest
-	 * \param ansi
+	 * \brief Dumps the memory in the range from \p src to \p src + \p size.
+	 * \details Writes the dump to the file specified by \p dest.
+	 * \param src The starting address to dump from.
+	 * \param size The number of bytes to dump.
+	 * \param columns The number of columns to display.
+	 * \param columnsize The number of bytes per column.
+	 * \param dest A pointer to the `FILE` to dump to.
+	 * \param ansi Whether or not to enhance the output using ANSI escape codes.
 	 * \return
+	 *		`#LS_E_NULL` if \p src or \p dest is `NULL`.					<br/>
+	 *		`#LS_E_NOOP` if \p size is less than 1.							<br/>
+	 *		`#LS_E_INVALID` if \p columns or \p columnsize is less than 1.	<br/>
+	 *		`#LS_E_IO_WRITE` if writing to \p dest fails.					<br/>
+	 *		`#LS_E_IO_FLUSH` if flushing to \p dest fails.					<br/>
+	 *		`#LS_E_SUCCESS` otherwise.
 	 */
 	LSAPI ls_result_t ls_memory_dump_ex(const void *const src, const size_t size, const unsigned int columns, const unsigned int columnsize, FILE *const dest, const ls_bool_t ansi);
 
 	/*!
-	 * \brief ls_memory_dump
-	 * \param src
-	 * \param size
+	 * \brief \copybrief ls_memory_dump_ex
+	 * \details Writes the dump to `stdout` using 16 single-byte columns.
+	 * \param src The starting address to dump from.
+	 * \param size The number of bytes to dump.
 	 * \return
+	 *		`#LS_E_NULL` if \p src or \p dest is `NULL`.					<br/>
+	 *		`#LS_E_NOOP` if \p size is less than 1.							<br/>
+	 *		`#LS_E_IO_WRITE` if writing to `stdout` fails.					<br/>
+	 *		`#LS_E_IO_FLUSH` if flushing to `stdout` fails.					<br/>
+	 *		`#LS_E_SUCCESS` otherwise.
 	 */
 	LSAPI ls_result_t ls_memory_dump(const void *const src, const size_t size);
 
 	/*!
-	 * \brief ls_memory_to_c_array
-	 * \param data
-	 * \param size
+	 * \brief Converts the memory in the range from \p src to \p src + \p size to a string representing the memory as a C array of bytes.
+	 * \details Each successful call to this function must be countered by a call to `free` once, to deallocate the allocated memory.
+	 * \param src The starting address to dump from.
+	 * \param size The number of bytes to dump.
 	 * \return
+	 *		`NULL` on failure.	<br/>
+	 *		A pointer to the newly allocated string otherwise.
 	 */
-	LSAPI char* ls_memory_to_c_array(const void *const data, const size_t size);
+	LSAPI char* ls_memory_to_c_array(const void *const src, const size_t size);
 
 	/*!
-	 * \brief ls_memory_free
-	 * \param target
-	 * \return
+	 * \brief Wrapper for the standard `free` function.
+	 * \details Unlike standard `free`, this function always returns `NULL` and checks if the passed pointer isn't null before passing it down.
+	 * \param target The pointer to the memory to deallocate.
+	 * \return `NULL`, always.
 	 */
 	LSAPI void* ls_memory_free(void *const target);
 
 	/*!
-	 * \brief ls_memory_clear
-	 * \param target
-	 * \param size
+	 * \brief Clears the memory in the range from \p target to \p target + \p size by filling it up with zeroes.
+	 * \param target The pointer to the memory to clear.
+	 * \param size The number of bytes to clear.
 	 */
 	LSAPI void ls_memory_clear(volatile void *const target, const size_t size);
 
 	/*!
-	 * \brief ls_memory_clear_free
-	 * \param target
-	 * \param size
-	 * \return
+	 * \brief Clears and deallocates memory by combining calls to `#ls_memory_clear` and `#ls_memory_free`.
+	 * \param target The pointer to the memory to clear.
+	 * \param size The number of bytes to clear.
+	 * \return `NULL`, always.
 	 */
 	LSAPI void* ls_memory_clear_free(volatile void *const target, const size_t size);
 
