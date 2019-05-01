4b419af3c74fd134fb1906d00adfc4e68b2caf49
ice cube tray <icecubetray@users.noreply.github.com>
2019-05-02 00:45:30 +0200

    Revamp: fix parallelism in bootstrapping

--- build/bootstrap.mk
+++ build/bootstrap.mk
@@ -3,6 +3,6 @@ include build/functions.mk build/configuration.mk
 # If nothing is specified, automagically run parallel
 parallel:
 	@echo Running with -j$(PARALLELISM_CORES)
-	@$(MAKE) -f build/bootstrap.mk --no-print-directory -j$(PARALLELISM_CORES)
+	@$(MAKE) -f build/bootstrap.mk --no-print-directory -j$(PARALLELISM_CORES) all
 
 include build/variables.mk build/compilation.mk build/kickoff.mk
