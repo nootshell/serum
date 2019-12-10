0574751278e543a166f0874133df40f0bad67689
icecubetray <icecubetray@users.noreply.github.com>
2019-12-10 23:09:16 +0100

    Add sanity checking base

--- src/core/sanity.h
+++ src/core/sanity.h
@@ -1,5 +1,29 @@
-/* core/platform.h */
-#ifdef __SERUM_CORE_PLATFORM_H
+#ifndef __SERUM_CORE_SANITY_H
+#define __SERUM_CORE_SANITY_H
+
+
+
+
+#include <errno.h>
+
+
+
+
+#define SERUM_OK							0
+#define SERUM_ERRNO							1	/*!< \hideinitializer \brief Returned when errno describes the error perfectly fine. */
+#define SERUM_NULLPTR						2	/*!< \hideinitializer \brief Nullpointer encountered where NULL was not expected. */
+
+
+
+
+#define SERUM_RETERRNOIFEXPR(__expr, __ret, __errno)	\
+	if ((__expr)) {										\
+		*errno = (__errno);								\
+		return (__ret);									\
+	}
+
+#define SERUM_CHECK_NULLPTR(__ptr)			SERUM_RETERRNOIFEXPR(((__ptr) == NULL), SERUM_NULLPTR, ENOMSG)
+
 
 
 
