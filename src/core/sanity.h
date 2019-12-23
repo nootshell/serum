ed63adf96dbdbae49c0e80187390eb2620e9ccee
icecubetray <icecubetray@users.noreply.github.com>
2019-12-23 05:36:51 +0100

    Add range checking

--- src/core/sanity.h
+++ src/core/sanity.h
@@ -12,6 +12,7 @@
 #define SERUM_OK							0
 #define SERUM_ERRNO							1	/*!< \hideinitializer \brief Returned when errno describes the error perfectly fine. */
 #define SERUM_NULLPTR						2	/*!< \hideinitializer \brief Nullpointer encountered where NULL was not expected. */
+#define SERUM_RANGE							3	/*!< \hideinitializer \brief Value is out of the expected range. */
 
 
 
@@ -23,6 +24,7 @@
 	}
 
 #define SERUM_CHECK_NULLPTR(__ptr)			SERUM_RETERRNOIFEXPR(((__ptr) == NULL), SERUM_NULLPTR, ENOMSG)
+#define SERUM_CHECK_RANGE(__val, _lb, _ub)	SERUM_RETERRNOIFEXPR(( ( (__val) < (_lb) ) || ( (__val) > (_ub) ) ), SERUM_RANGE, ENOMSG);
 
 
 
